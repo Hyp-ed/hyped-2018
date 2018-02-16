ab4c6bac88f8679657939889abfaa2ead4558516
Ragnor Comerford <ragnor.comerford@gmail.com>
2018-02-16 16:49:10 +0000

    Fix Travis CI build error

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -13,7 +13,7 @@ HOST_DEPENDENCIES="debootstrap qemu-user-static binfmt-support sbuild"
 GUEST_DEPENDENCIES="build-essential git m4 sudo python"
 
 # Command used to run the tests
-TEST_COMMAND="make test"
+TEST_COMMAND="cd BeagleBone_black && g++ main.cpp -o main"
 
 function setup_arm_chroot {
     # Host dependencies
