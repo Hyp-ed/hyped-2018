4abb075673f2a84fc24d9e9cfcb27e6c295ac9b9
Sean Mullan <seanmullan97@outlook.com>
2018-03-11 21:02:28 +0000

    Refactor style

--- BeagleBone_black/src/data/data.hpp
+++ BeagleBone_black/src/data/data.hpp
@@ -6,16 +6,16 @@
  *
  *    Copyright 2018 HYPED
  *    Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain a
- * copy of the License at
+ *    not use this file except in compliance with the License. You may obtain a
+ *    copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
  *    Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *    License for the specific language governing permissions and limitations
+ *    under the License.
  */
 
 #ifndef BEAGLEBONE_BLACK_DATA_DATA_HPP_
@@ -133,6 +133,7 @@ class Data {
    * @brief      Retrieves data produced by navigation sub-team.
    */
   Navigation getNavigationData();
+
   /**
    * @brief      Should be called by navigation sub-team whenever they have new data.
    */
@@ -171,7 +172,6 @@ class Data {
   Lock lock_motors_;
 };
 
-}  // namespace data
-}  // namespace hyped
+}}  // namespace data::hyped
 
 #endif  // BEAGLEBONE_BLACK_DATA_DATA_HPP_
