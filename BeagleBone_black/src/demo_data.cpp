21938cc0d9b1be440082b4a0fa8503906274d8a5
Sean Mullan <seanmullan97@outlook.com>
2018-03-15 22:35:47 +0000

    Fix merge conflict

--- BeagleBone_black/src/demo_data.cpp
+++ BeagleBone_black/src/demo_data.cpp
@@ -37,7 +37,7 @@ int main()
   printf("Hello hyped, here are some shared data\n");
   printf("from Navigation: %d %d %d\n", nav.distance, nav.velocity, nav.acceleration);
   printf("from Sensors: %d %d %d at time %d\n",
-      sens.imu[0].acc_x, sens.proxy[0].val, sens.stripe_cnt.value, sens.stripe_cnt.timestamp);
+      sens.imu[0].acc.value[0], sens.proxy[0].val, sens.stripe_cnt.value, sens.stripe_cnt.timestamp);
   printf("from Motors: FL %d, FR %d, BL %d, BR %d\n",
       mot.rpm_FL, mot.rpm_FR, mot.rpm_BL, mot.rpm_BR);
   return 0;
