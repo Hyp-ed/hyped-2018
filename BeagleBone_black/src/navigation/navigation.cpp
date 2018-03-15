650722ab7bad7fe6e62bbf3f3593483732602596
Uday Patel <udaykpatel@hotmail.com>
2018-03-15 18:56:01 +0000

    Add navigation class skeleton

---
+++ BeagleBone_black/src/navigation/navigation.cpp
@@ -0,0 +1,51 @@
+/*
+ * Author: Adithya Sireesh
+ * Organisation: HYPED
+ * Date: 10/02/2018
+ * Description:
+ *
+ *    Copyright 2018 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "navigation.hpp"
+
+// Public methods
+void Navigation::update() {}
+
+float Navigation::get_accleration()
+{
+  return 0;
+}
+
+float Navigation::get_velocity()
+{
+  return 0;
+}
+
+float Navigation::get_displacement()
+{
+  return 0;
+}
+
+// Private methods
+
+void Navigation::gyro_update() {}
+
+void Navigation::acclerometer_update() {}
+
+void Navigation::proximity_orientation_update() {}
+
+void Navigation::proximity_displacement_update() {}
+
+void Navigation::stripe_counter_update() {}
---
+++ BeagleBone_black/src/navigation/navigation.hpp
@@ -0,0 +1,63 @@
+/*
+ * Author:
+ * Organisation: HYPED
+ * Date:
+ * Description:
+ *
+ *    Copyright 2018 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#ifndef HYPED_NAVIGATION_NAVIGATION_HPP_
+#define HYPED_NAVIGATION_NAVIGATION_HPP_
+
+#include <cstdint>
+#include "utils/math/kalman.hpp"
+#include "utils/math/vector.hpp"
+
+using hyped::utils::math::Kalman;
+using hyped::utils::math::Vector;
+
+class Navigation {
+ public:
+  // TODO(Uday,Brano,Adi): Change the return values for the private methods to vector/quaternion
+
+  void update();
+  float get_accleration();
+  float get_velocity();
+  float get_displacement();
+
+ private:
+  // TODO(Uday,Brano,Adi): Add the arguements for the private methods
+
+  void gyro_update();  // Point number 1
+  void acclerometer_update();  // Point number 3, 4, 5 and 6
+  void proximity_orientation_update();  // Point number 7
+  void proximity_displacement_update();  // Point number 7
+  void stripe_counter_update();  // Point number 7
+  // float velocity_update();  // Point number 4 and 5
+
+  // TODO(Uday,Brano,Adi): Change the data types of the data members
+
+  float accleration_;
+  float velocity_;
+  float displacement_;
+  float orientation_;
+  int stripe_count_;
+  Kalman<Vector<int16_t, 3>> acclerometer_filter_;
+  Kalman<Vector<int16_t, 3>> gyro_filter_;
+  // TODO(Uday,Brano,Adi): Decide the type
+  Kalman<uint8_t> proximity_filter_;
+};
+
+#endif  // HYPED_NAVIGATION_NAVIGATION_HPP_
