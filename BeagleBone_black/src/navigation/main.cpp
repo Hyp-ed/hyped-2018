782158d8b06c646cebf61a3c5d262124589e18da
Branislav Pilňan <s1556515@sms.ed.ac.uk>
2018-03-25 18:17:34 +0100

    Navigation basic skeleton (#48)
    * Add navigation class skeleton
    
    * Update style
    
    * Add main navigation thread
    
    * Add navigation files
    
    * Update description, name and style
    
    * Added namespace for navigation files
    
    * Update constructor method to support Vector class
    
    * Update navigation files
    
    * Update reference to be a constant reference
    
    * Refactor some declarations from cpp file to hpp file
    
    * Add argument for update function
    
    * Refactor function calls
    
    * Implement basic logic and data flow in the nav thread
    The main nav thread now reads and writes the shared data. There is also
    a basic skeleton for triggering appropriate nav calculations.
    
    * Implement basic skeleton of navigation algorithm
    This skeleton should be able to process accelerometer data correctly,
    producing velocity and displacement. It doesn't use filtering and
    ignores all other data so it it will be very imprecise. It is only
    useful to test the data flow in our system.
    
    * Nav refractor kalman (#47)
    
    * Fill up description of Kalman class
    
    * Refactor Kalman Class
    
    * Revert "Refactor Kalman Class"
    
    This reverts commit 62535db159ed951439f7bd030410fbacacef0acc.
    
    * Remove estimation error from the constructor
    
    * Fix issues
    
    * Add comments and fix types
    Acceleration and angular velocity are expected to be negative at times.
    While velocity and displacement should not be negative during the
    hyperloop run, it could theoretically happen. Anyway, it's better not to
    mix signed and unsigned integers so I made them all signed.
    
    * Reformat the license comment
    
    * Merge changes from develop
    
    To get up-to-date version of the main `main.cpp`
    
    * Add navigation thread to the main `main.cpp`

---
+++ BeagleBone_black/src/navigation/main.cpp
@@ -0,0 +1,80 @@
+/*
+ * Author: Brano, Adi, Uday
+ * Organisation: HYPED
+ * Date: 18 March 2018
+ * Description: Main file for navigation class.
+ *
+ *    Copyright 2018 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ *    except in compliance with the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *    either express or implied. See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "navigation/main.hpp"
+
+namespace hyped {
+
+using data::Sensors;
+
+namespace navigation {
+
+Main::Main(uint8_t id) : Thread(id), data_(data::Data::getInstance()), nav_()
+{/* EMPTY */}
+
+void Main::run()
+{
+  data::Navigation nav_data;
+  Sensors last_readings = data_.getSensorsData();  // TODO(Brano): Make sure data_ is properly initd
+  while (1) {
+    Sensors readings = data_.getSensorsData();
+
+    // TODO(Brano): Accelerations and gyros should be in separate arrays in data::Sensors.
+    if (!imuChanged(last_readings, readings))
+      continue;
+    if (proxiChanged(last_readings, readings) && stripeCntChanged(last_readings, readings))
+      nav_.update(readings.imu, readings.proxy, readings.stripe_cnt);
+    else if (proxiChanged(last_readings, readings))
+      nav_.update(readings.imu, readings.proxy);
+    else if (stripeCntChanged(last_readings, readings))
+      nav_.update(readings.imu, readings.stripe_cnt);
+    else
+      nav_.update(readings.imu);
+
+    nav_data.distance = nav_.get_displacement();
+    nav_data.velocity = nav_.get_velocity();
+    nav_data.acceleration = nav_.get_accleration();
+    // TODO(Brano): Add stripe count or remove it from data::Navigation.
+    data_.setNavigationData(nav_data);
+  }
+}
+
+bool Main::imuChanged(const Sensors& old_data, const Sensors& new_data)
+{
+  for (unsigned int i = 0; i < new_data.imu.size(); ++i)
+    if (new_data.imu[i].gyr.timestamp != old_data.imu[i].gyr.timestamp ||
+        new_data.imu[i].acc.timestamp != old_data.imu[i].acc.timestamp)
+      return true;
+  return false;
+}
+
+bool Main::proxiChanged(const Sensors& old_data, const Sensors& new_data)
+{
+  for (unsigned int i = 0; i < new_data.proxy.size(); ++i)
+    // TODO(Brano): Timestamp proxi data in data::Sensors
+    if (new_data.proxy[i].val != old_data.proxy[i].val)
+      return true;
+  return false;
+}
+
+inline bool Main::stripeCntChanged(const Sensors& old_data, const Sensors& new_data)
+{
+  return new_data.stripe_cnt.timestamp != old_data.stripe_cnt.timestamp;
+}
+
+}}  // namespace hyped::navigation
