168915b6ad0d291ce456a2f146d6c0a06eb8e2f4
Edekje <ethanvanwoerkom@yahoo.com>
2018-04-02 15:38:44 +0100

    Compile passing I2C thread

--- BeagleBone_black/src/demo_i2c.cpp
+++ BeagleBone_black/src/demo_i2c.cpp
@@ -20,38 +20,17 @@
  *    limitations under the License.
  */
 
-#include <stdint.h>
-#include "i2c/i2c.hpp"
+#include <iostream>
+#include "utils/io/i2c/i2c.hpp"
 
-using namespace hyped;
+using hyped::utils::concurrent::Thread;
+using hyped::utils::io::I2c;
+using namespace std;
 
-void initialiseSlaves(i2c::Driver* instance)
-{
-  /* What this does is slave dependent, suggested for VL6180X:
-  Turn on/off GPIO pins to boot
-  Set registers to continuous operation mode with given frequency
-  Get test values to establish working
-  */
-}
-
-int main(void)
-{
-  i2c::Driver device1 = i2c::Driver();
-  /* Replace with your application code */
-
-  // Set up context for driver configuration:
-  i2c::Context device1_settings;  // Must be filled appropriately
-
-  device1.configure(&device1_settings);
-  initialiseSlaves(&device1);       // Will perform real I2C operations
-  i2c::Channel inputs[10];          // This should be further initialised
-  device1.configureChannels(inputs, 10);   // Requires no communications
-
-  while (1) {
-    device1.control();
-    // It is suggested that in the while loop the health
-    // of the I2C connection is monitored by checking error counters and statuses
-    // Meanwhile, Control() will be automatically running communications
-    // and calling callback functions to make aware of new results
-  }
+int main(){
+	Thread* i2c_thread = new I2c(17); // 17 as I2C thread number
+	i2c_thread->run();
+	i2c_thread->join();
+	cout << "Thread joined and executed" << endl;
+	return 0;
 }
