9667841a3d122586ac1e50f30e304e319c079cdd
Uday Patel <udaykpatel@hotmail.com>
2018-05-28 12:23:06 +0100

    Update class to support method

--- BeagleBone_black/src/sensors/fake_imu.cpp
+++ BeagleBone_black/src/sensors/fake_imu.cpp
@@ -18,28 +18,69 @@
  *    limitations under the License.
  */
 
+#include <random>
+
+#include <fstream>
 #include <string>
+
 #include "data/data.hpp"
+#include "data/data_point.hpp"
+#include "sensors/fake_imu.hpp"
 
 namespace hyped {
 
 using data::Imu;
+using data::NavigationType;
+using data::NavigationVector;
+using data::DataPoint;
 
 namespace sensors {
 
-void FakeImu::getData(Imu* imu)
+FakeImu::FakeImu(NavigationVector acc_val, NavigationType acc_noise,
+                 NavigationVector gyr_val, NavigationType gyr_noise) :
+  acc_val(acc_val), gyr_val(gyr_val),
+  acc_noise(acc_noise), gyr_noise(gyr_noise)
 {
-  // TODO(Uday): Fill in method
+  setData();
 }
 
-void FakeImu::addNoiseToData(Imu* imu)
+void FakeImu::setData()
 {
-  // TODO(Uday): Fill in method
+  imu_.acc.value = acc_val;
+  imu_.gyr.value = gyr_val;
 }
 
-void FakeImu::readDataFromFile(Imu* imu)
+Imu FakeImu::getData()
 {
-  // TODO(Uday): Fill in method
+  Imu temp;
+  temp.acc = DataPoint<NavigationVector>(imu_.acc.timestamp,
+                                         imu_.acc.value + addNoiseToData(acc_val, acc_noise));
+  temp.gyr = DataPoint<NavigationVector>(imu_.gyr.timestamp,
+                                         imu_.gyr.value + addNoiseToData(gyr_val, gyr_noise));
+  return temp;
+}
+
+NavigationVector FakeImu::addNoiseToData(NavigationVector value, NavigationType noise)
+{
+  NavigationVector temp;
+  std::default_random_engine generator;
+
+  for (int i = 0; i < 3; i++) {
+    std::normal_distribution<double> distribution(value[i], noise);
+    temp[i] = distribution(generator);
+  }
+
+  return temp;
+}
+
+void FakeImu::readDataFromFile(std::string file_path)
+{
+  std::ifstream file;
+  file.open(file_path);
+  while (!file.eof()) {
+    // TODO(Uday): Implement this
+  }
+  file.close();
 }
 
 }}  // namespace hyped::sensors
