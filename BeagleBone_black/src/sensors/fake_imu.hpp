50c665b2862b64e478612fd4374344b2f2510b44
Uday Patel <udaykpatel@hotmail.com>
2018-06-09 10:13:05 +0800

    Update comments

--- BeagleBone_black/src/sensors/fake_imu.hpp
+++ BeagleBone_black/src/sensors/fake_imu.hpp
@@ -37,6 +37,11 @@ using std::chrono::high_resolution_clock;
 
 namespace sensors {
 
+/*
+ * @brief    This class is to imitate an IMU. This works by calling the constructor once
+ *           and calling getData function multiple times at different time periods to produce
+ *           reading that will be used by other classes.
+ */
 class FakeImu : public ImuInterface {
  public:
   /*
@@ -44,21 +49,31 @@ class FakeImu : public ImuInterface {
    *
    * @param[in]    The line format of the input file would be the following
    *
-   *               timestamp value_x value_y value_z
-   *                         noise_x noise_y noise_z
+   *               timestamp value_x value_y value_z noise_x noise_y noise_z
    *
-   *               Sample of the format is located at 'src/fake_imu_input_xxx.txt'
+   *               Sample of the format is located at 'src/fake_imu_input_xxx.txt'. Note that the
+   *               timestamp for accelerometer has to start with 0 and must be multiples of 250 and 
+   *               125 for accelerometer and gyroscope respectively. You must include every timestamp
+   *               from 0 to the last timestamp which will be a multiple of 250 or 125 depending on
+   *               if it is an accelerometer or gyroscope.
+   *
+   * @param[in] acc_file_path    A string to the file location of the accelerometer data points
+   * @param[in] gyr_file_path    A string to the file location of the gyroscope data points
    */
   explicit FakeImu(std::string acc_file_path, std::string gyr_file_path);
 
   /*
-   * @brief     A constructor for the fake IMU class
+   * @brief     A constructor for the fake IMU class. This works by generating random numbers
+   *            using a normal distribution with xxx_val as mean and xxx_noise as standard deviation.
    */
   explicit FakeImu(NavigationVector acc_val, NavigationVector acc_noise,
                    NavigationVector gyr_val, NavigationVector gyr_noise);
 
   /*
-   * @brief     A function that gets the imu data
+   * @brief     A function that gets the imu data at the time of call. The function will return
+   *            the same data point if the time period since the last update isn't long enough. It
+   *            will also skip a couple of data points if the time since the last call has been
+   *            sufficiently long.
    */
   void getData(Imu* imu) override;
 
@@ -67,7 +82,7 @@ class FakeImu : public ImuInterface {
   const uint64_t kGyrTimeInterval = 125;
 
   /*
-   * @brief     A function that initializes the variables
+   * @brief     A function that initializes the common variables
    */
   void setData();
 
@@ -79,12 +94,18 @@ class FakeImu : public ImuInterface {
   void readDataFromFile(std::string acc_file_path, std::string gyr_file_path);
 
   /*
-   * @brief     A function that adds noise to the imu data
+   * @brief     A function that adds noise to the imu data using normal distribution
+   *
+   * @param[in] value    This is the mean of the normal distribution
+   * @param[in] noise    This is the standard deviation of the normal distribution
+   *
+   * @return    Returns random data point value
    */
   static NavigationVector addNoiseToData(NavigationVector value, NavigationVector noise);
 
   /*
-   * @brief     Checks to see if sufficient time has pass for the sensor to be updated
+   * @brief     Checks to see if sufficient time has pass for the sensor to be updated and checks if
+   *            some data points need to be skipped
    */
   bool accCheckTime();
   bool gyrCheckTime();
