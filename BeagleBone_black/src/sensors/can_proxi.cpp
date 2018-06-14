0022c59eb8eb7c8be208fd80f022d2d1f6be7242
Martin Kristien <martin.kristien@gmail.com>
2018-06-14 17:20:54 +0100

    Create CanProxi device to get proxi data from SAMC21 board

---
+++ BeagleBone_black/src/sensors/can_proxi.cpp
@@ -0,0 +1,109 @@
+/*
+ * Author: Martin Kristien
+ * Organisation: HYPED
+ * Date: 08/06/18
+ * Description: Implements proximity driver over CANBUS using
+ * data from SAMC21 board
+ *
+ *    Copyright 2018 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "sensors/can_proxi.hpp"
+
+
+#include "utils/system.hpp"
+
+namespace hyped {
+namespace sensors {
+
+
+bool    CanProxi::can_registered = false;
+bool    CanProxi::valid[kNumProximities] = {};
+uint8_t CanProxi::data[kNumProximities] = {};
+static_assert(kNumProximities <= 8, "cannot use more than 8 proxies over CAN");
+
+CanProxi::CanProxi(uint8_t id): CanProxi(id, utils::System::getLogger())
+{ /* EMPTY */ }
+
+CanProxi::CanProxi(uint8_t id, Logger& log)
+    : log_(log),
+      id_(id)
+{
+  if (!can_registered) {
+    for (auto& v : valid) {
+      v = false;
+    }
+
+    utils::io::Can::getInstance().registerProcessor(this);
+    can_registered = true;
+  }
+}
+
+// ---------------------------------------------------------------------
+// ProxiInterface
+// ---------------------------------------------------------------------
+bool CanProxi::isOnline()
+{
+  return valid[id_];
+}
+void CanProxi::getData(Proximity* proxi)
+{
+  proxi->val = data[id_];
+}
+
+// ---------------------------------------------------------------------
+// CanProcessor
+// ---------------------------------------------------------------------
+void CanProxi::processNewData(Frame& message)
+{
+  switch (message.id) {
+    case 0x45A: {   // echo reply
+      // TODO(anyone): verify reply has the correct values (for debugging)
+      break;
+    }
+    case 0x45B: {   // data message
+      uint8_t size = kNumProximities < message.len ? kNumProximities : message.len;
+      for (uint8_t i = 0; i < size; i++) {
+        data[i] = message.data[i];
+      }
+      break;
+    }
+    case 0x45C: {   // proxi health status
+      uint8_t is_online = message.data[0];
+      for (uint8_t i = 0; i < kNumProximities; i++) {
+        valid[i] = is_online & (1 << i);
+      }
+      break;
+    }
+    default: {
+      log_.ERR("CanProxi", "received unsupported message with id %u", message.id);
+    }
+  }
+}
+
+bool CanProxi::hasId(uint32_t id, bool extended)
+{
+  switch (id) {
+    case 0x45A:
+    case 0x45B:
+    case 0x45C:
+      return true;
+    default:
+      return false;
+  }
+}
+
+
+}}  // namespace hyped::sensors
+
---
+++ BeagleBone_black/src/sensors/can_proxi.hpp
@@ -0,0 +1,79 @@
+/*
+ * Author: Martin Kristien
+ * Organisation: HYPED
+ * Date: 08/06/18
+ * Description: Implements proximity driver over CANBUS using
+ * data from SAMC21 board
+ *
+ *    Copyright 2018 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#ifndef BEAGLEBONE_BLACK_SENSORS_CAN_PROXI_HPP_
+#define BEAGLEBONE_BLACK_SENSORS_CAN_PROXI_HPP_
+
+#include <cstdint>
+
+#include "data/data.hpp"
+#include "sensors/interface.hpp"
+#include "utils/io/can.hpp"
+
+
+namespace hyped {
+
+// namespace utils { namespace io { class Can; }}
+namespace utils { class Logger; }
+
+using utils::io::can::Frame;
+using utils::io::CanProccesor;
+using utils::Logger;
+
+namespace sensors {
+
+constexpr uint32_t kNumProximities = data::Sensors::kNumProximities;
+
+
+/**
+ * @brief Represents and instance of a proximity sensor connected to SAMC21 board.
+ * The values are transfered over CANBUS.
+ *
+ * Note, all 8 values are transfered as a single CAN message with a single ID.
+ * Therefore, only 1 object handles CAN message processing and puts the data to
+ * the static data[] variable for all other proximity objects to share.
+ */
+class CanProxi : public ProxiInterface, public CanProccesor {
+ public:
+  explicit CanProxi(uint8_t id);  // id must be < data::Sensors::kNumProximities
+  CanProxi(uint8_t id, Logger& log);
+
+  bool isOnline() override;
+  void getData(Proximity* proxi) override;
+
+  void processNewData(Frame& message);
+  bool hasId(uint32_t id, bool extended);
+
+
+
+ private:
+  Logger& log_;
+  uint8_t id_;
+
+  static bool    can_registered;
+  static bool    valid[kNumProximities];
+  static uint8_t data[kNumProximities];
+};
+
+}}  // namespace hyped::sensors
+
+#endif  // BEAGLEBONE_BLACK_SENSORS_CAN_PROXI_HPP_
+
