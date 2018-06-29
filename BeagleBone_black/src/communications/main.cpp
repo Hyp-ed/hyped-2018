98907b848486b38c4924285264d045850d47c193
Isabella Chan <isabellachan426@gmail.com>
2018-06-29 17:43:32 +0100

    refactor sender

--- BeagleBone_black/src/communications/main.cpp
+++ BeagleBone_black/src/communications/main.cpp
@@ -21,7 +21,6 @@
 #include "communications/main.hpp"
 
 #include <string>
-
 #include <sstream>
 
 namespace hyped {
@@ -155,8 +154,8 @@ int Main::sendTorqueBl(float torquebl)
   return baseCommunicator_->sendData("CMD20" + std::to_string(torquebl) + "\n");
 }
 
-int Main::sendImu(bool op, bool op1, bool op2,
-bool op3, bool op4, bool op5, bool op6, bool op7)
+int Main::sendImu(bool op, bool op1, bool op2, bool op3,
+                  bool op4, bool op5, bool op6, bool op7)
 {
   std::string sen, sen1, sen2, sen3, sen4, sen5, sen6, sen7;
   sen = op ? "1" : "2";
@@ -167,12 +166,12 @@ bool op3, bool op4, bool op5, bool op6, bool op7)
   sen5 = op5 ? "1" : "2";
   sen6 = op6 ? "1" : "2";
   sen7 = op7 ? "1" : "2";
-  return baseCommunicator_->sendData("CMD21" + sen + sen1 +
-  sen2 + sen3 + sen4 + sen5 + sen6 + sen7 + "\n");
+  return baseCommunicator_->sendData("CMD21" + sen + sen1 + sen2 +
+                                     sen3 + sen4 + sen5 + sen6 + sen7 + "\n");
 }
 
-int Main::sendProxiFront(bool op, bool op1, bool op2,
-bool op3, bool op4, bool op5, bool op6, bool op7)
+int Main::sendProxiFront(bool op, bool op1, bool op2, bool op3,
+                         bool op4, bool op5, bool op6, bool op7)
 {
   std::string sen, sen1, sen2, sen3, sen4, sen5, sen6, sen7;
   sen = op ? "1" : "2";
@@ -183,12 +182,12 @@ bool op3, bool op4, bool op5, bool op6, bool op7)
   sen5 = op5 ? "1" : "2";
   sen6 = op6 ? "1" : "2";
   sen7 = op7 ? "1" : "2";
-  return baseCommunicator_->sendData("CMD22" + sen + sen1 +
-  sen2 + sen3 + sen4 + sen5 + sen6 + sen7 + "\n");
+  return baseCommunicator_->sendData("CMD22" + sen + sen1 + sen2 +
+                                     sen3 + sen4 + sen5 + sen6 + sen7 + "\n");
 }
 
-int Main::sendProxiRear(bool op, bool op1, bool op2,
-bool op3, bool op4, bool op5, bool op6, bool op7)
+int Main::sendProxiRear(bool op, bool op1, bool op2, bool op3,
+                        bool op4, bool op5, bool op6, bool op7)
 {
   std::string sen, sen1, sen2, sen3, sen4, sen5, sen6, sen7;
   sen = op ? "1" : "2";
@@ -199,8 +198,8 @@ bool op3, bool op4, bool op5, bool op6, bool op7)
   sen5 = op5 ? "1" : "2";
   sen6 = op6 ? "1" : "2";
   sen7 = op7 ? "1" : "2";
-  return baseCommunicator_->sendData("CMD23" + sen + sen1 +
-  sen2 + sen3 + sen4 + sen5 + sen6 + sen7 + "\n");
+  return baseCommunicator_->sendData("CMD23" + sen + sen1 + sen2 +
+                                     sen3 + sen4 + sen5 + sen6 + sen7 + "\n");
 }
 
 void Main::run()
@@ -239,17 +238,17 @@ void Main::run()
     sendTorqueBr(mtr_.torque_4);
     sendTorqueBl(mtr_.torque_3);
     sendImu(sen_.imu.value[0].operational, sen_.imu.value[1].operational,
-    sen_.imu.value[2].operational, sen_.imu.value[3].operational,
-    sen_.imu.value[4].operational, sen_.imu.value[5].operational,
-    sen_.imu.value[6].operational, sen_.imu.value[7].operational);
+            sen_.imu.value[2].operational, sen_.imu.value[3].operational,
+            sen_.imu.value[4].operational, sen_.imu.value[5].operational,
+            sen_.imu.value[6].operational, sen_.imu.value[7].operational);
     sendProxiFront(sen_.proxi_front.value[0].operational, sen_.proxi_front.value[1].operational,
-    sen_.proxi_front.value[2].operational, sen_.proxi_front.value[3].operational,
-    sen_.proxi_front.value[4].operational, sen_.proxi_front.value[5].operational,
-    sen_.proxi_front.value[6].operational, sen_.proxi_front.value[7].operational);
+                   sen_.proxi_front.value[2].operational, sen_.proxi_front.value[3].operational,
+                   sen_.proxi_front.value[4].operational, sen_.proxi_front.value[5].operational,
+                   sen_.proxi_front.value[6].operational, sen_.proxi_front.value[7].operational);
     sendProxiRear(sen_.proxi_back.value[0].operational, sen_.proxi_back.value[1].operational,
-    sen_.proxi_back.value[2].operational, sen_.proxi_back.value[3].operational,
-    sen_.proxi_back.value[4].operational, sen_.proxi_back.value[5].operational,
-    sen_.proxi_back.value[6].operational, sen_.proxi_back.value[7].operational);
+                  sen_.proxi_back.value[2].operational, sen_.proxi_back.value[3].operational,
+                  sen_.proxi_back.value[4].operational, sen_.proxi_back.value[5].operational,
+                  sen_.proxi_back.value[6].operational, sen_.proxi_back.value[7].operational);
   }
 
   receiverThread->join();
