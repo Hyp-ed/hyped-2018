06367128e186af79a028131bba85f5f557771c19
jackhorsburgh <jackhorse1000@gmail.com>
2018-07-03 16:04:26 +0100

    Debug

--- BeagleBone_black/src/demo_multiplexer.cpp
+++ BeagleBone_black/src/demo_multiplexer.cpp
@@ -24,12 +24,16 @@
 #include "utils/system.hpp"
 #include "utils/concurrent/thread.hpp"
 #include "sensors/interface.hpp"
+#include "utils/math/statistics.hpp"
 
 using hyped::sensors::VL6180;
 using hyped::utils::Logger;
 using hyped::utils::concurrent::Thread;
 using hyped::utils::io::I2C;
 using hyped::sensors::ProxiInterface;
+using hyped::utils::math::RollingStatistics;
+
+ constexpr uint8_t kNumOfProxis = 3;
 
 int main(int argc, char* argv[])
 {
@@ -38,8 +42,8 @@ int main(int argc, char* argv[])
   Logger log(true, 1);
   log.INFO("TEST-vl6180", "VL6180 instance successfully created");
   uint8_t kMultiplexerAddr = 0x70;
-  uint8_t kNumOfProxis = 3;
-  ProxiInterface* proxi_[kNumOfProxis];
+  VL6180* proxi_[kNumOfProxis];
+  array<RollingStatistics<float>, kNumOfProxis> stats = {RollingStatistics<float>(10), RollingStatistics<float>(10), RollingStatistics<float>(10)};
 
   // i2c.write(kMultiplexerAddr, 0xFF);
   for (int i = 0; i < kNumOfProxis; i++) {
@@ -47,7 +51,7 @@ int main(int argc, char* argv[])
       log.INFO("Multiplexer", "Opening channel: %d", i);
       VL6180* proxi = new VL6180(0x29, log);
       proxi->setContinuousRangingMode();
-      // proxi->setAddress(0x29);
+      proxi->setAddress(0x29 + i);
       proxi_[i] = proxi;
     }
    // i2c.write(kMultiplexerAddr, 0xFF);
@@ -57,7 +61,11 @@ int main(int argc, char* argv[])
     for (int j = 0; j < kNumOfProxis; j++) {
       i2c.write(kMultiplexerAddr, 0x01 << j);  // open particular i2c channel
       hyped::data::Proximity proxi;
-      proxi_[i%kNumOfProxis]->getData(&proxi);
+      proxi_[j]->getData(&proxi);
+      stats[j].update(static_cast<float>(proxi.val));
+      if (stats[j].getVariance() == 0.0) {
+        log.ERR("Variance", "0!");
+      }
       log.INFO("Multiplexer-test", "Sensor %d, reading %d", j, proxi.val);
       Thread::sleep(10);
     }
