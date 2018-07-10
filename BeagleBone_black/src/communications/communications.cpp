f354c02053fe8d90d4afecbad19470c9d93b4e28
Isabella Chan <isabellachan426@gmail.com>
2018-07-10 22:30:08 +0100

    Add connection flag

--- BeagleBone_black/src/communications/communications.cpp
+++ BeagleBone_black/src/communications/communications.cpp
@@ -36,12 +36,14 @@ Communications::Communications(Logger& log, const char* ip, int portNo)
   struct hostent *server;
 
   if (sockfd_ < 0) {
+    connected_ = false;
     log_.ERR("COMN", "CANNOT OPEN SOCKET.");
   }
 
   server = gethostbyname(ip);
 
   if (server == NULL) {
+    connected_ = false;
     log_.ERR("COMN", "INCORRECT BASE-STATION IP, OR BASE-STATION S/W NOT RUNNING.");
   }
 
@@ -51,12 +53,13 @@ Communications::Communications(Logger& log, const char* ip, int portNo)
   serv_addr.sin_port = htons(portNo);
 
   if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
-    log_.ERR("COMN", "Invalid address.\n");
+    log_.ERR("COMN", "INVALID ADDRESS.\n");
+    connected_ = false;
   }
 
   if (connect(sockfd_, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-    log_.ERR("COMN", "CANNOT ESTABLISH CONNECTION TO BASE-STATION.");
     // if connect does not complete successfully it returns -1
+    log_.ERR("COMN", "CANNOT ESTABLISH CONNECTION TO BASE-STATION.");
     connected_ = false;
   } else {
     log_.INFO("COMN", "TCP/IP connection established.");
