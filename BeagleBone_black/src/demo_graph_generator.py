661fb97bc29a4d522324ae6884c4e7b2c2d72d21
Uday Patel <udaykpatel@hotmail.com>
2018-07-19 21:47:42 +0800

    Add graph generator based on files

---
+++ BeagleBone_black/src/demo_graph_generator.py
@@ -0,0 +1,27 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
+std = 0
+total = 0
+
+random_values = []
+real_values = []
+filter_values = []
+
+with open('data.txt') as input_file:
+  total, std = [float(x) for x in next(input_file).split()]
+  for _ in range(0, int(total)):
+    real_values.append([float(x) for x in next(input_file).split()])
+  for _ in range(0, int(total)):
+    random_values.append([float(x) for x in next(input_file).split()])
+    
+plt.plot(random_values, label = 'random values')
+plt.plot(real_values, label = 'real_values')
+plt.plot(filter_values, label = 'filtered values')
+
+plt.legend()
+
+plt.show()
+
